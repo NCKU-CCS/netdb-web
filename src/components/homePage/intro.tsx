13c6c7ca61d795506a948b01ad6d56f42f2411ea
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-10 01:44:54 +0800

    feat: intro bg

--- src/components/homePage/intro.tsx
+++ src/components/homePage/intro.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Intro: React.FC = () => (
-  <div className={classNames('homepage-intro')}>intro</div>
+  <div className={classNames('homepage-intro')}>
+    intro
+    <img alt="intro" src={`${process.env.PUBLIC_URL}/homePage/intro.png`} />
+  </div>
 );
 
 export default Intro;
