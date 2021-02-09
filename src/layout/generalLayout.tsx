92d3516fcf44245aa779768d1835e3176b0c8aea
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-09 18:55:18 +0800

    feat: general layout

---
+++ src/layout/generalLayout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const GeneralLayout: React.FC = () => (
+  <main className={classNames('general-layout')}>
+    general layout
+  </main>
+);
+
+export default GeneralLayout;
