0ba63dec1705251932013ac1b32dff7609f67f5f
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-10 00:56:20 +0800

    feat: footer done

--- src/components/footer/links.tsx
+++ src/components/footer/links.tsx
@@ -6,8 +6,8 @@ const Links: React.FC = () => (
   <div className={classNames('footer-links')}>
     <FooterLink path="/" name="Home" />
     <FooterLink path="/work" name="Our Works" />
-    <FooterLink path="/advisor" name="Advisor" />
-    <FooterLink path="/member" name="Members" />
+    <FooterLink path="/advisor" name="Netdb Lab Advisor" />
+    <FooterLink path="/member" name="Netdb Lab Members" />
     <FooterLink path="/contact" name="Contact Us" />
   </div>
 );
