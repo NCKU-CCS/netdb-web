5e785ad5a19bb5a90ad658c5907032f153b6dc49
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-10 23:04:51 +0800

    feat: google map iframe

--- src/components/contactPage/index.tsx
+++ src/components/contactPage/index.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import Map from './map';
 
 const ContactPageContainer: React.FC = () => (
-  <div>
+  <>
     contact page
     <Map />
-  </div>
+  </>
 );
 
 export default ContactPageContainer;
