5580c31f46fec4cb0264c2a7200425e2506230f5
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-10 20:46:53 +0800

    feat: contact page prototype

---
+++ src/components/contactPage/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Map from './map';
+
+const ContactPageContainer: React.FC = () => (
+  <div>
+    contact page
+    <Map />
+  </div>
+);
+
+export default ContactPageContainer;
---
+++ src/components/contactPage/map.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Map: React.FC = () => <div>map</div>;
+
+export default Map;
