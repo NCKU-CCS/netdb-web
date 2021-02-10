14e6144207035ef5f8b02a485bff41ff5451f987
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-10 17:34:33 +0800

    feat: service

--- src/components/homePage/index.tsx
+++ src/components/homePage/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Intro from './intro';
 import Partner from './partner';
 import Research from './research';
+import Service from './service';
 
 const HomePageContainer = () => (
   <>
     <Intro />
     <Partner />
     <Research />
+    <Service />
   </>
 );
 
