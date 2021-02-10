5b8648fad453fdc2b66d9eafff2275d078a7553a
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-10 16:36:42 +0800

    feat: research domain

--- src/components/homePage/index.tsx
+++ src/components/homePage/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Intro from './intro';
 import Partner from './partner';
+import Research from './research';
 
 const HomePageContainer = () => (
   <>
     <Intro />
     <Partner />
+    <Research />
   </>
 );
 
---
+++ src/components/homePage/research.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import classNames from 'classnames';
+import Domain from './domain';
+
+const Research: React.FC = () => (
+  <div className={classNames('homepage-research')}>
+    Research Domain
+    <div className={classNames('homepage-research-domain-container')}>
+      <Domain
+        imgName="data-science"
+        title="Data Science"
+        content="From data collection to data analytics, we are making an effort to build the complete data pipeline of knowledge discovery in the big data environment. We can innovate the new method and adopt it to the certain scenario through trial-and-error rapidly."
+      />
+      <Domain
+        imgName="cloud-computing"
+        title="Cloud Computing"
+        content="We aim at building cross-domain cloud infrastructure and integrating different micro-services in the current agile development environment with high efficiency and high availability."
+      />
+      <Domain
+        imgName="DLT"
+        title="DLT"
+        content="By exploring distributed ledger technology (DLT), our researches is approaching to find a way to stand with the data sovereignty while providing non-repudiation to the user in the decentralized digital world."
+      />
+      <Domain
+        imgName="AIOT"
+        title="AIOT"
+        content="Our goal is to advance the evolution of life landscape by combining artificial intelligent and IOT. By leveraging real-time connectivity of IOT device,we develop edge computing available algorithm which can change your living behavior and toward a more smarter community."
+      />
+    </div>
+  </div>
+);
+
+export default Research;
