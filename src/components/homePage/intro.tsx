8fb2361cc5aabbd46266981a973a940c4f9ee624
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-13 00:50:18 +0800

    feat: add packages and into animation

--- src/components/homePage/intro.tsx
+++ src/components/homePage/intro.tsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import classNames from 'classnames';
 
-const Intro: React.FC = () => (
-  <div className={classNames('homepage-intro')}>
-    <div className={classNames('homepage-intro-title-container')}>
-      <div className={classNames('homepage-intro-title')}>
-        Delivering innovation from data
+const Intro: React.FC = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <div className={classNames('homepage-intro')}>
+      <div
+        className={classNames('homepage-intro-title-container')}
+        data-aos="fade-right"
+        data-aos-easing="ease-in"
+      >
+        <div className={classNames('homepage-intro-title')}>
+          Delivering innovation from data
+        </div>
+        <div className={classNames('homepage-intro-subtitle')}>
+          Liberate the value of data, Empower the trust of data and Leverage
+          data science to facilitate society
+        </div>
       </div>
-      <div className={classNames('homepage-intro-subtitle')}>
-        Liberate the value of data, Empower the trust of data and Leverage data
-        science to facilitate society
-      </div>
+      <img
+        alt="intro"
+        src={`${process.env.PUBLIC_URL}/homePage/intro/intro.png`}
+        data-aos="fade-left"
+        data-aos-easing="ease-in"
+      />
     </div>
-    <img
-      alt="intro"
-      src={`${process.env.PUBLIC_URL}/homePage/intro/intro.png`}
-    />
-  </div>
-);
+  );
+};
 
 export default Intro;
