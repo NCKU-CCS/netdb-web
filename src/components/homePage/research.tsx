54e4419bb681e42631855a089941b02bad22fcb6
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-13 01:50:15 +0800

    fix: animation and layout minor fixes

--- src/components/homePage/research.tsx
+++ src/components/homePage/research.tsx
@@ -1,33 +1,43 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import classNames from 'classnames';
 import Domain from './domain';
 
-const Research: React.FC = () => (
-  <div className={classNames('homepage-research')}>
-    Research Domain
-    <div className={classNames('homepage-research-domain-container')}>
-      <Domain
-        imgName="data-science"
-        title="Data Science"
-        content="From data collection to data analytics, we are making an effort to build the complete data pipeline of knowledge discovery in the big data environment. We can innovate the new method and adopt it to the certain scenario through trial-and-error rapidly."
-      />
-      <Domain
-        imgName="cloud-computing"
-        title="Cloud Computing"
-        content="We aim at building cross-domain cloud infrastructure and integrating different micro-services in the current agile development environment with high efficiency and high availability."
-      />
-      <Domain
-        imgName="DLT"
-        title="DLT"
-        content="By exploring distributed ledger technology (DLT), our researches is approaching to find a way to stand with the data sovereignty while providing non-repudiation to the user in the decentralized digital world."
-      />
-      <Domain
-        imgName="AIOT"
-        title="AIOT"
-        content="Our goal is to advance the evolution of life landscape by combining artificial intelligent and IOT. By leveraging real-time connectivity of IOT device,we develop edge computing available algorithm which can change your living behavior and toward a more smarter community."
-      />
+const Research: React.FC = () => {
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+  return (
+    <div className={classNames('homepage-research')}>
+      <div data-aos="fade-up" data-aos-easing="ease-in" data-aos-duration="500">
+        Research Domain
+      </div>
+      <div className={classNames('homepage-research-domain-container')}>
+        <Domain
+          imgName="data-science"
+          title="Data Science"
+          content="From data collection to data analytics, we are making an effort to build the complete data pipeline of knowledge discovery in the big data environment. We can innovate the new method and adopt it to the certain scenario through trial-and-error rapidly."
+        />
+        <Domain
+          imgName="cloud-computing"
+          title="Cloud Computing"
+          content="We aim at building cross-domain cloud infrastructure and integrating different micro-services in the current agile development environment with high efficiency and high availability."
+        />
+        <Domain
+          imgName="DLT"
+          title="DLT"
+          content="By exploring distributed ledger technology (DLT), our researches is approaching to find a way to stand with the data sovereignty while providing non-repudiation to the user in the decentralized digital world."
+        />
+        <Domain
+          imgName="AIOT"
+          title="AIOT"
+          content="Our goal is to advance the evolution of life landscape by combining artificial intelligent and IOT. By leveraging real-time connectivity of IOT device,we develop edge computing available algorithm which can change your living behavior and toward a more smarter community."
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Research;
