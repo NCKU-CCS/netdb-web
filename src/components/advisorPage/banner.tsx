9b5b050c6118444c9717feaeea646e34e64e2ae1
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-16 19:34:15 +0800

    feat: banner done

--- src/components/advisorPage/banner.tsx
+++ src/components/advisorPage/banner.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Banner: React.FC = () => (
-  <div className={classNames('advisorpage-banner')}>advisor banner</div>
+  <div className={classNames('advisorpage-banner')}>
+    <h1>NetDB Lab Advisor</h1>
+    <img
+      alt="NetDB Lab Advisor"
+      src={`${process.env.PUBLIC_URL}/advisorPage/bg.png`}
+    />
+  </div>
 );
 
 export default Banner;
