fc8a05c4ecc4a3db6a735e21d1a86ba27bdbbf2f
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-17 15:49:42 +0800

    feat: change banner pic from png to svg

--- src/components/advisorPage/banner.tsx
+++ src/components/advisorPage/banner.tsx
@@ -6,7 +6,7 @@ const Banner: React.FC = () => (
     <h1>NetDB Lab Advisor</h1>
     <img
       alt="NetDB Lab Advisor"
-      src={`${process.env.PUBLIC_URL}/advisorPage/bg.png`}
+      src={`${process.env.PUBLIC_URL}/advisorPage/bg.svg`}
     />
   </div>
 );
