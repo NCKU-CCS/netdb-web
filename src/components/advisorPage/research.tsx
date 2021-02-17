6d0b03757f6536b771d13e5e6db38a32b3a6d228
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-17 18:31:34 +0800

    feat: research done

--- src/components/advisorPage/research.tsx
+++ src/components/advisorPage/research.tsx
@@ -2,7 +2,33 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Research: React.FC = () => (
-  <div className={classNames('advisorpage-research')}>advisor research</div>
+  <div className={classNames('advisorpage-research')}>
+    <h3>Research Interest ＆ Teaching</h3>
+    <section>
+      <h4>Research Interest</h4>
+      <ul className={classNames('advisorpage-research-interest-list')}>
+        <li> • Data Mining</li>
+        <li> • Web Intelligence</li>
+        <li> • Database</li>
+        <li> • Distributed Ledger Technology</li>
+        <li> • Cloud Computing</li>
+        <li> • Intelligent Engine Management</li>
+      </ul>
+    </section>
+    <section>
+      <h4>Teaching</h4>
+      <ul className={classNames('advisorpage-research-teaching-list')}>
+        <li>
+          2015/9 - 2016/1 &nbsp; Introduction to Know. Discovery and Data
+          Engineering
+        </li>
+        <li>2015/2 - 2015/6 &nbsp; Theory of Computation</li>
+        <li>
+          2014/9 - 2015/2 &nbsp; Introduction to Cloud Computing and System
+        </li>
+      </ul>
+    </section>
+  </div>
 );
 
 export default Research;
