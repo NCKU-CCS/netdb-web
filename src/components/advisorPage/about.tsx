dc3ece0b601d7c86eae57316d0e57b182a3d2d93
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-17 16:52:12 +0800

    feat: about top

--- src/components/advisorPage/about.tsx
+++ src/components/advisorPage/about.tsx
@@ -2,7 +2,32 @@ import React from 'react';
 import classNames from 'classnames';
 
 const About: React.FC = () => (
-  <div className={classNames('advisorpage-about')}>advisor about</div>
+  <div className={classNames('advisorpage-about')}>
+    <div className={classNames('advisorpage-about-top-container')}>
+      <div className={classNames('advisorpage-about-top-text')}>
+        <h3>Kun-Ta Chuang (莊坤達)</h3>
+        <div>Assistant Professor</div>
+        <div>
+          Tainan City, Taiwan Dept. of Computer Science and Information
+          Engineering, National Cheng Kung University
+        </div>
+        <a href="mailto:ktchuang@mail.ncku.edu.tw">
+          <img
+            alt="email"
+            src={`${process.env.PUBLIC_URL}/advisorPage/mail.svg`}
+          />
+          ktchuang@mail.ncku.edu.tw
+        </a>
+      </div>
+      <img
+        alt="Kun-Ta Chuang (莊坤達)"
+        src={`${process.env.PUBLIC_URL}/advisorPage/ktchuang.jpeg`}
+      />
+    </div>
+    <div className={classNames('advisorpage-about-bottom-container')}>
+      About me
+    </div>
+  </div>
 );
 
 export default About;
