24a5ec551d691a5e3c1ecd11fbcf978db2346fb7
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-20 15:42:19 +0800

    feat: banner done

--- src/components/memberPage/banner.tsx
+++ src/components/memberPage/banner.tsx
@@ -2,7 +2,16 @@ import React from 'react';
 import classNames from 'classnames';
 
 const Banner: React.FC = () => (
-  <div className={classNames('memberpage-banner')}>Banner</div>
+  <div className={classNames('memberpage-banner')}>
+    <h1>
+      NetDB
+      <br /> Lab Members
+    </h1>
+    <img
+      alt="NetDB Lab Advisor"
+      src={`${process.env.PUBLIC_URL}/memberPage/bg.svg`}
+    />
+  </div>
 );
 
 export default Banner;
