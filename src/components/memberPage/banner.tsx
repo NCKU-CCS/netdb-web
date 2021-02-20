7244b00cd2a057a6ed80599a635d876f9249968e
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-02-20 14:59:58 +0800

    feat: styles file

--- src/components/memberPage/banner.tsx
+++ src/components/memberPage/banner.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import classNames from 'classnames';
 
-const Banner: React.FC = () => <div>Banner</div>;
+const Banner: React.FC = () => (
+  <div className={classNames('memberpage-banner')}>Banner</div>
+);
 
 export default Banner;
