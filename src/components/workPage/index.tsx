e3ef9a6f62304ca5d31c9486c4fa2eb022239c78
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-03-02 20:08:23 +0800

    feat: prototype

---
+++ src/components/workPage/index.tsx
@@ -0,0 +1,8 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+
+const WorkPageContainer: React.FC = () => {
+  return <div>WorkPageContainer</div>;
+};
+
+export default WorkPageContainer;
