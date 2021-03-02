7456ae0b3fdc82788525d04754e5ece8fe49fbe9
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-03-02 23:49:55 +0800

    feat: hover effect can stop on specific btn

--- src/components/workPage/year.tsx
+++ src/components/workPage/year.tsx
@@ -1,20 +1,43 @@
 /* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 
-const Year: React.FC = () => (
-  <div className={classNames('workpage-year')}>
-    <div className={classNames('workpage-year-buttons')}>
-      <button type="button">2021</button>
-      <button type="button">2020</button>
-      <button type="button">2019</button>
-      <button type="button">2018</button>
-      <button type="button">2017</button>
-      <button type="button">2016</button>
-      <button type="button">2015</button>
-      <button type="button" className={classNames('workpage-year-bar')} />
+const Year: React.FC = () => {
+  const [btnClicked, setBtnClicked] = useState<number>(1);
+
+  return (
+    <div className={classNames('workpage-year')}>
+      <div className={classNames('workpage-year-buttons')}>
+        <button type="button" onClick={() => setBtnClicked(1)}>
+          2021
+        </button>
+        <button type="button" onClick={() => setBtnClicked(2)}>
+          2020
+        </button>
+        <button type="button" onClick={() => setBtnClicked(3)}>
+          2019
+        </button>
+        <button type="button" onClick={() => setBtnClicked(4)}>
+          2018
+        </button>
+        <button type="button" onClick={() => setBtnClicked(5)}>
+          2017
+        </button>
+        <button type="button" onClick={() => setBtnClicked(6)}>
+          2016
+        </button>
+        <button type="button" onClick={() => setBtnClicked(7)}>
+          2015
+        </button>
+        <div
+          className={classNames(
+            'workpage-year-bar',
+            `workpage-year-bar--${btnClicked}`,
+          )}
+        />
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Year;
