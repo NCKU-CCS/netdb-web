a05e55488cc498b881820a8791a0504c82750646
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-03-04 21:25:29 +0800

    feat: html tag of project

--- src/components/workPage/index.tsx
+++ src/components/workPage/index.tsx
@@ -1,14 +1,31 @@
 import React, { useState } from 'react';
 import Banner from './banner';
 import Year from './year';
+import Project from './project';
+import projects from './projects.json';
 
 const WorkPageContainer: React.FC = () => {
-  const [year, setYear] = useState<number>(2021);
+  const [year, setYear] = useState<number>(2015);
+
+  const createProjects = projects
+    .filter((d) => d.year === year)
+    .map((d) => (
+      <Project
+        name={d.name}
+        type={d.type}
+        slogan={d.slogan}
+        introduction={d.introduction}
+        link={d.link}
+        img={d.img}
+        partners={d.partners}
+      />
+    ));
 
   return (
     <>
       <Banner />
       <Year year={year} setYear={setYear} />
+      {createProjects}
     </>
   );
 };
--- src/components/workPage/project.tsx
+++ src/components/workPage/project.tsx
@@ -1,6 +1,47 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from 'react';
-// import classNames from 'classnames';
+import classNames from 'classnames';
 
-const Project: React.FC = () => <div>Project</div>;
+interface IProps {
+  name: string;
+  type: string;
+  slogan: string;
+  introduction: string;
+  link: string;
+  img: string;
+  partners: string[];
+}
+
+const Project: React.FC<IProps> = ({
+  name,
+  type,
+  slogan,
+  introduction,
+  link,
+  img,
+  partners,
+}) => (
+  <div className={classNames('workpage-project')}>
+    <div className={classNames('workpage-project-text')}>
+      <div>
+        <img alt="eye" />
+        <div>
+          {slogan}
+          <div>
+            {name}
+            <img alt="fb" />
+          </div>
+        </div>
+      </div>
+      <p>{introduction}</p>
+      <div>
+        {partners.map((d) => (
+          <img alt={d} />
+        ))}
+      </div>
+    </div>
+    <img alt={name} />
+  </div>
+);
 
 export default Project;
