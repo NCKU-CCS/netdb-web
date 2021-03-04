bf5adb8b9daaefa677c7cf47af4fb724991db400
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-03-04 23:46:08 +0800

    feat: project card almost done

--- src/components/workPage/project.tsx
+++ src/components/workPage/project.tsx
@@ -23,24 +23,36 @@ const Project: React.FC<IProps> = ({
 }) => (
   <div className={classNames('workpage-project')}>
     <div className={classNames('workpage-project-text')}>
-      <div>
-        <img alt="eye" />
-        <div>
-          {slogan}
-          <div>
+      <div className={classNames('workpage-project-text-header')}>
+        <img alt="eye" src={`${process.env.PUBLIC_URL}/workPage/eye.svg`} />
+        <div className={classNames('workpage-project-text-header-right')}>
+          <div
+            className={classNames('workpage-project-text-header-right-slogan')}
+          >
+            {slogan}
+          </div>
+          <div
+            className={classNames('workpage-project-text-header-right-name')}
+          >
             {name}
-            <img alt="fb" />
+            <img
+              alt="fb"
+              src={`${process.env.PUBLIC_URL}/workPage/website.png`}
+            />
           </div>
         </div>
       </div>
       <p>{introduction}</p>
-      <div>
+      <div className={classNames('workpage-project-text-footer')}>
         {partners.map((d) => (
-          <img alt={d} />
+          <img
+            alt={d}
+            src={`${process.env.PUBLIC_URL}/workPage/台南市政府.jpg`}
+          />
         ))}
       </div>
     </div>
-    <img alt={name} />
+    <img alt={img} src={`${process.env.PUBLIC_URL}/workPage/taiwan-stat.png`} />
   </div>
 );
 
