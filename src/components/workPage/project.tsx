b1016e9f8abf0be5f8680dd9eed3b132a4da91ca
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-03-05 15:57:12 +0800

    feat: project title

--- src/components/workPage/project.tsx
+++ src/components/workPage/project.tsx
@@ -21,40 +21,48 @@ const Project: React.FC<IProps> = ({
   img,
   partners,
 }) => (
-  <div className={classNames('workpage-project')}>
-    <div className={classNames('workpage-project-text')}>
-      <div className={classNames('workpage-project-text-header')}>
-        <img alt="eye" src={`${process.env.PUBLIC_URL}/workPage/eye.svg`} />
-        <div className={classNames('workpage-project-text-header-right')}>
-          <div
-            className={classNames('workpage-project-text-header-right-slogan')}
-          >
-            {slogan}
-          </div>
-          <div
-            className={classNames('workpage-project-text-header-right-name')}
-          >
-            {name}
-            <a href={link}>
-              <img
-                alt="fb"
-                src={`${process.env.PUBLIC_URL}/workPage/website.png`}
-              />
-            </a>
+  <div className={classNames('workpage-project-container')}>
+    <h4>Project on {type}</h4>
+    <div className={classNames('workpage-project')}>
+      <div className={classNames('workpage-project-text')}>
+        <div className={classNames('workpage-project-text-header')}>
+          <img alt="eye" src={`${process.env.PUBLIC_URL}/workPage/eye.svg`} />
+          <div className={classNames('workpage-project-text-header-right')}>
+            <div
+              className={classNames(
+                'workpage-project-text-header-right-slogan',
+              )}
+            >
+              {slogan}
+            </div>
+            <div
+              className={classNames('workpage-project-text-header-right-name')}
+            >
+              {name}
+              <a href={link}>
+                <img
+                  alt="fb"
+                  src={`${process.env.PUBLIC_URL}/workPage/website.png`}
+                />
+              </a>
+            </div>
           </div>
         </div>
+        <p>{introduction}</p>
+        <div className={classNames('workpage-project-text-footer')}>
+          {partners.map((d) => (
+            <img
+              alt={d}
+              src={`${process.env.PUBLIC_URL}/workPage/台南市政府.jpg`}
+            />
+          ))}
+        </div>
       </div>
-      <p>{introduction}</p>
-      <div className={classNames('workpage-project-text-footer')}>
-        {partners.map((d) => (
-          <img
-            alt={d}
-            src={`${process.env.PUBLIC_URL}/workPage/台南市政府.jpg`}
-          />
-        ))}
-      </div>
+      <img
+        alt={img}
+        src={`${process.env.PUBLIC_URL}/workPage/taiwan-stat.png`}
+      />
     </div>
-    <img alt={img} src={`${process.env.PUBLIC_URL}/workPage/taiwan-stat.png`} />
   </div>
 );
 
