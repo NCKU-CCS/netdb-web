fef6714c563f2d6d2fc07465e1adf249de23576f
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-06-24 15:44:25 +0800

    Updates

---
+++ precache-manifest.bcb70eb6466cc43d2c8bbf3254920cbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "572ebe0ae0cb490905829dd117ed24d6",
+    "url": "/netdb-web/index.html"
+  },
+  {
+    "revision": "d1287a6e922cb7dd9f65",
+    "url": "/netdb-web/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "458a1a4d0ef9697a4b78",
+    "url": "/netdb-web/static/css/main.125c74a0.chunk.css"
+  },
+  {
+    "revision": "d1287a6e922cb7dd9f65",
+    "url": "/netdb-web/static/js/2.bbac0dc4.chunk.js"
+  },
+  {
+    "revision": "44ba416b9a7217ca99c025d9f4dd1124",
+    "url": "/netdb-web/static/js/2.bbac0dc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f42f7d1102b72ad0ac96",
+    "url": "/netdb-web/static/js/3.8843927b.chunk.js"
+  },
+  {
+    "revision": "458a1a4d0ef9697a4b78",
+    "url": "/netdb-web/static/js/main.a9710fd9.chunk.js"
+  },
+  {
+    "revision": "23dcafdd7b75077068e2",
+    "url": "/netdb-web/static/js/runtime-main.7adfd6c9.js"
+  }
+]);
