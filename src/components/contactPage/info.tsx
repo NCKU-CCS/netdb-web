ddd5eb54c6a7b427b904033b461bbd527e16316d
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-08-14 09:47:48 +0800

    feat: contact page RWD

--- src/components/contactPage/info.tsx
+++ src/components/contactPage/info.tsx
@@ -3,36 +3,61 @@ import classNames from 'classnames';
 
 const Info: React.FC = () => (
   <div className={classNames('contactpage-info')}>
-    <h1>Contact Us</h1>
+    <div className={classNames('contactpage-info-title')}>Contact Us</div>
     <div className={classNames('contactpage-info-content')}>
-      <img alt="netdb" src={`${process.env.PUBLIC_URL}/contactPage/icon.png`} />
-      <div className={classNames('contactpage-info-location')}>
-        <img
-          alt="location"
-          src={`${process.env.PUBLIC_URL}/contactPage/location.png`}
-        />
-        <p>
-          Dept. of Computer Science and Information Engineering
-          <br /> Advanced Network Database Lab (NCKU Netdb Lab)
-          <br /> 1 University Road, Tainan, Taiwan 701
-        </p>
-      </div>
-      <div className={classNames('contactpage-info-contact')}>
-        <div className={classNames('contactpage-info-contact-item')}>
+      <img
+        className={classNames('contactpage-info-netdb-logo')}
+        alt="netdb"
+        src={`${process.env.PUBLIC_URL}/contactPage/icon.png`}
+      />
+      <div className={classNames('contactpage-info-right-container')}>
+        <div className={classNames('contactpage-info-border')} />
+        <div className={classNames('contactpage-info-location')}>
           <img
-            alt="phone"
-            src={`${process.env.PUBLIC_URL}/contactPage/phone.png`}
+            className={classNames('contactpage-info-location-img')}
+            alt="location"
+            src={`${process.env.PUBLIC_URL}/contactPage/location.png`}
           />
-          <a href="tel:+886-6-2757575">+886-6-2757575 ext. 62520 # 2608</a>
+          <div className={classNames('contactpage-info-location-text')}>
+            <div className={classNames('contactpage-info-location-text-item')}>
+              Dept. of Computer Science and Information Engineering
+            </div>
+            <div className={classNames('contactpage-info-location-text-item')}>
+              Advanced Network Database Lab (NCKU Netdb Lab)
+            </div>
+            <div className={classNames('contactpage-info-location-text-item')}>
+              1 University Road, Tainan, Taiwan 701
+            </div>
+          </div>
         </div>
-        <div className={classNames('contactpage-info-contact-item')}>
-          <img
-            alt="mail"
-            src={`${process.env.PUBLIC_URL}/contactPage/mail.png`}
-          />
-          <a href="mailto:netdb@netdb.csie.ncku.edu.tw">
-            netdb @ netdb.csie.ncku.edu.tw
-          </a>
+        <div className={classNames('contactpage-info-border')} />
+        <div className={classNames('contactpage-info-contact')}>
+          <div className={classNames('contactpage-info-contact-item')}>
+            <img
+              className={classNames('contactpage-info-contact-item-img')}
+              alt="phone"
+              src={`${process.env.PUBLIC_URL}/contactPage/phone.png`}
+            />
+            <a
+              className={classNames('contactpage-info-contact-item-link')}
+              href="tel:+886-6-2757575"
+            >
+              +886-6-2757575 ext. 62520 # 2608
+            </a>
+          </div>
+          <div className={classNames('contactpage-info-contact-item')}>
+            <img
+              className={classNames('contactpage-info-contact-item-img')}
+              alt="mail"
+              src={`${process.env.PUBLIC_URL}/contactPage/mail.png`}
+            />
+            <a
+              className={classNames('contactpage-info-contact-item-link')}
+              href="mailto:netdb@netdb.csie.ncku.edu.tw"
+            >
+              netdb@netdb.csie.ncku.edu.tw
+            </a>
+          </div>
         </div>
       </div>
     </div>
