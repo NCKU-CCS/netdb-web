fa97bac55f1e542a7531ed1cb165872592098d5d
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-08-15 22:40:10 +0800

    feat: loading bar

--- src/layouts/generalLayout.tsx
+++ src/layouts/generalLayout.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
+import LinearProgress from '@material-ui/core/LinearProgress';
+
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
 
@@ -7,12 +9,24 @@ interface IProps {
   children: React.ReactNode;
 }
 
-const GeneralLayout: React.FC<IProps> = ({ children }) => (
-  <main className={classNames('general-layout')}>
-    <Navbar />
-    {children}
-    <Footer />
-  </main>
-);
+const GeneralLayout: React.FC<IProps> = ({ children }) => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  return (
+    <>
+      {isLoading && (
+        <LinearProgress className={classNames('general-layout-progress')} />
+      )}
+      <main
+        className={classNames('general-layout', { loading: isLoading })}
+        onLoad={() => setIsLoading(false)}
+      >
+        <Navbar />
+        {children}
+        <Footer />
+      </main>
+    </>
+  );
+};
 
 export default GeneralLayout;
