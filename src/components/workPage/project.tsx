f64b57d8d82639f721b6ab27e473768ee9c51b4a
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-08-15 22:04:34 +0800

    feat: img expand btn

--- src/components/workPage/project.tsx
+++ src/components/workPage/project.tsx
@@ -1,6 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
+import Button from '@material-ui/core/Button';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
 interface IProps {
   name: string;
@@ -20,73 +22,91 @@ const Project: React.FC<IProps> = ({
   link,
   img,
   partners,
-}) => (
-  <div className={classNames('workpage-project-container')}>
-    <div className={classNames('workpage-project-category')}>
-      Project on {type}
-    </div>
-    <div className={classNames('workpage-project')}>
-      <div className={classNames('workpage-project-card-left-container')}>
-        <div className={classNames('workpage-project-text-header')}>
-          <img
-            className={classNames('workpage-project-card-left-img')}
-            alt={type}
-            src={`${process.env.PUBLIC_URL}/workPage/icons/${type}.svg`}
-          />
-          <div className={classNames('workpage-project-text-header-right')}>
-            <div
-              className={classNames(
-                'workpage-project-text-header-right-slogan',
-              )}
-            >
-              {slogan}
-            </div>
-            <div
-              className={classNames('workpage-project-text-header-right-name')}
-            >
-              {name}
-              {/* {link && (
-                <a href={link}>
-                  <img
-                    alt="fb"
-                    src={`${process.env.PUBLIC_URL}/workPage/website.png`}
-                  />
-                </a>
-              )} */}
+}) => {
+  const [isImgExpanded, setIsImgExpanded] = useState(false);
+
+  return (
+    <div className={classNames('workpage-project-container')}>
+      <div className={classNames('workpage-project-category')}>
+        Project on {type}
+      </div>
+      <div className={classNames('workpage-project')}>
+        <div className={classNames('workpage-project-card-left-container')}>
+          <div className={classNames('workpage-project-text-header')}>
+            <img
+              className={classNames('workpage-project-card-left-img')}
+              alt={type}
+              src={`${process.env.PUBLIC_URL}/workPage/icons/${type}.svg`}
+            />
+            <div className={classNames('workpage-project-text-header-right')}>
+              <div
+                className={classNames(
+                  'workpage-project-text-header-right-slogan',
+                )}
+              >
+                {slogan}
+              </div>
+              <div
+                className={classNames(
+                  'workpage-project-card-header-name-container',
+                )}
+              >
+                <div>{name}</div>
+                {/* {link && (
+                  <a
+                    className={classNames('workpage-project-card-right-link')}
+                    href={link}
+                  >
+                    <img
+                      className={classNames('workpage-project-card-right-link-img')}
+                      alt="fb"
+                      src={`${process.env.PUBLIC_URL}/workPage/website.png`}
+                    />
+                  </a>
+                )} */}
+              </div>
             </div>
           </div>
+          <p>{introduction}</p>
+          <div className={classNames('workpage-project-text-footer')}>
+            {partners.map((d) => (
+              <img
+                alt={d}
+                src={`${process.env.PUBLIC_URL}/workPage/partners/${d}`}
+              />
+            ))}
+          </div>
         </div>
-        <p>{introduction}</p>
-        <div className={classNames('workpage-project-text-footer')}>
-          {partners.map((d) => (
-            <img
-              alt={d}
-              src={`${process.env.PUBLIC_URL}/workPage/partners/${d}`}
-            />
-          ))}
-        </div>
-      </div>
-      <div className={classNames('workpage-project-card-right-container')}>
-        {link && (
-          <a
-            className={classNames('workpage-project-card-right-link')}
-            href={link}
+        <div className={classNames('workpage-project-card-right-container')}>
+          <Button
+            className={classNames('workpage-project-card-right-expand-btn')}
+            onClick={() => setIsImgExpanded(!isImgExpanded)}
           >
-            <img
-              className={classNames('workpage-project-card-right-link-img')}
-              alt="fb"
-              src={`${process.env.PUBLIC_URL}/workPage/website.png`}
-            />
-          </a>
-        )}
-        <img
-          className={classNames('workpage-project-card-img')}
-          alt={img}
-          src={`${process.env.PUBLIC_URL}/workPage/pics/${img}.png`}
-        />
+            {isImgExpanded ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+          </Button>
+          {/* {link && (
+            <a
+              className={classNames('workpage-project-card-right-link')}
+              href={link}
+            >
+              <img
+                className={classNames('workpage-project-card-right-link-img')}
+                alt="fb"
+                src={`${process.env.PUBLIC_URL}/workPage/website.png`}
+              />
+            </a>
+          )} */}
+          <img
+            className={classNames('workpage-project-card-img', {
+              expanded: isImgExpanded,
+            })}
+            alt={img}
+            src={`${process.env.PUBLIC_URL}/workPage/pics/${img}.png`}
+          />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Project;
