afc95e7b774cc8fb35293a7a2b38246fca65e5af
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-08-16 14:46:58 +0800

    feat: new year selector ui

--- src/components/workPage/year.tsx
+++ src/components/workPage/year.tsx
@@ -1,72 +1,44 @@
 import React from 'react';
 import classNames from 'classnames';
 
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
 interface IProps {
   year: number;
   setYear(y: number): void;
 }
 
-const Year: React.FC<IProps> = ({ year, setYear }) => (
-  <div className={classNames('workpage-year')}>
-    <div className={classNames('workpage-year-buttons')}>
-      <button
-        type="button"
-        className={classNames(year === 2021 ? 'chosen' : '')}
-        onClick={() => setYear(2021)}
-      >
-        2021
-      </button>
-      <button
-        type="button"
-        className={classNames(year === 2020 ? 'chosen' : '')}
-        onClick={() => setYear(2020)}
-      >
-        2020
-      </button>
-      <button
-        type="button"
-        className={classNames(year === 2019 ? 'chosen' : '')}
-        onClick={() => setYear(2019)}
-      >
-        2019
-      </button>
-      <button
-        type="button"
-        className={classNames(year === 2018 ? 'chosen' : '')}
-        onClick={() => setYear(2018)}
-      >
-        2018
-      </button>
-      <button
-        type="button"
-        className={classNames(year === 2017 ? 'chosen' : '')}
-        onClick={() => setYear(2017)}
-      >
-        2017
-      </button>
-      <button
-        type="button"
-        className={classNames(year === 2016 ? 'chosen' : '')}
-        onClick={() => setYear(2016)}
-      >
-        2016
-      </button>
-      <button
-        type="button"
-        className={classNames(year === 2015 ? 'chosen' : '')}
-        onClick={() => setYear(2015)}
-      >
-        2015
-      </button>
-      <div
-        className={classNames(
-          'workpage-year-bar',
-          `workpage-year-bar--${year}`,
-        )}
-      />
+const Year: React.FC<IProps> = ({ year, setYear }) => {
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setYear(newValue + 2015);
+  };
+
+  return (
+    <div className={classNames('workpage-year')}>
+      <AppBar position="static" color="transparent">
+        <Tabs
+          value={year - 2015}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="on"
+          centered
+        >
+          {/* <Tab label="2021" value={2022 - 2015} /> */}
+          <Tab label="2021" value={2021 - 2015} />
+          <Tab label="2020" value={2020 - 2015} />
+          <Tab label="2019" value={2019 - 2015} />
+          <Tab label="2018" value={2018 - 2015} />
+          <Tab label="2017" value={2017 - 2015} />
+          <Tab label="2016" value={2016 - 2015} />
+          <Tab label="2015" value={2015 - 2015} />
+        </Tabs>
+      </AppBar>
     </div>
-    <h5>{year}</h5>
-  </div>
-);
+  );
+};
 
 export default Year;
