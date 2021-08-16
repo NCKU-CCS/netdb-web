c8217909a0d6d79bfb6a3dfce7be12cd395f7490
curry0622 <58351529+curry0622@users.noreply.github.com>
2021-08-16 17:11:00 +0800

    feat: smooth scroll on all browsers

--- src/layouts/generalLayout.tsx
+++ src/layouts/generalLayout.tsx
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
+import smoothscroll from 'smoothscroll-polyfill';
 import Fab from '@material-ui/core/Fab';
+import Zoom from '@material-ui/core/Zoom';
 import LinearProgress from '@material-ui/core/LinearProgress';
-import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
 
 import Navbar from '../components/navbar';
 import Footer from '../components/footer';
@@ -14,12 +16,24 @@ interface IProps {
 const GeneralLayout: React.FC<IProps> = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   const [isRendered, setIsRendered] = useState(false);
+  const [scrollY, setScrollY] = useState(0);
+
+  const scrollToTop = () => {
+    smoothscroll.polyfill();
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
 
   useEffect(() => {
     if (!isLoading) {
       setTimeout(() => setIsRendered(true), 1000);
     }
   }, [isLoading]);
+
+  useEffect(() => {
+    const onScroll = () => setScrollY(window.pageYOffset);
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
 
   return (
     <>
@@ -32,18 +46,20 @@ const GeneralLayout: React.FC<IProps> = ({ children }) => {
       >
         <Navbar />
         {children}
-        <Fab
-          color="primary"
-          style={{
-            position: 'fixed',
-            bottom: '30px',
-            right: '30px',
-            backgroundColor: '#59d9ac',
-          }}
-          onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
-        >
-          <ArrowDropUpIcon />
-        </Fab>
+        <Zoom in={scrollY >= 50}>
+          <Fab
+            color="primary"
+            style={{
+              position: 'fixed',
+              bottom: '30px',
+              right: '30px',
+              backgroundColor: '#59d9ac',
+            }}
+            onClick={() => scrollToTop()}
+          >
+            <KeyboardArrowUpIcon />
+          </Fab>
+        </Zoom>
         <Footer />
       </main>
     </>
