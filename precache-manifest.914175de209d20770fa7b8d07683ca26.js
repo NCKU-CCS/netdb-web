2913349997160276d356a68597fe5c85969fb716
grandq33769 <grandq33769@gmail.com>
2021-11-25 08:50:58 +0000

    Deploying to gh-pages from @ NCKU-CCS/netdb-web@085bdf5d4b9c7c82e62bbcb45d6b7c3d0804d04a 🚀

--- precache-manifest.914175de209d20770fa7b8d07683ca26.js
+++ precache-manifest.914175de209d20770fa7b8d07683ca26.js
@@ -1,31 +1,31 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9917100949579a23e1f21148c6634928",
+    "revision": "b4a68a6e6d3e61976eb09009b4f7af0d",
     "url": "/netdb-web/index.html"
   },
   {
-    "revision": "76c042b682dbb0e816bf",
+    "revision": "6badc7f924d1dfedabf5",
     "url": "/netdb-web/static/css/2.98d1d9d4.chunk.css"
   },
   {
-    "revision": "5cdfc8e4e2b78879f04a",
+    "revision": "6ccf843abc758c2d2477",
     "url": "/netdb-web/static/css/main.d0ac810f.chunk.css"
   },
   {
-    "revision": "76c042b682dbb0e816bf",
-    "url": "/netdb-web/static/js/2.466c4579.chunk.js"
+    "revision": "6badc7f924d1dfedabf5",
+    "url": "/netdb-web/static/js/2.933aefe6.chunk.js"
   },
   {
     "revision": "1e5a6e613ee7bec103feb54c33c83c2c",
-    "url": "/netdb-web/static/js/2.466c4579.chunk.js.LICENSE.txt"
+    "url": "/netdb-web/static/js/2.933aefe6.chunk.js.LICENSE.txt"
   },
   {
     "revision": "25a11f0ddc46b8e5e933",
     "url": "/netdb-web/static/js/3.1d078097.chunk.js"
   },
   {
-    "revision": "5cdfc8e4e2b78879f04a",
-    "url": "/netdb-web/static/js/main.57214225.chunk.js"
+    "revision": "6ccf843abc758c2d2477",
+    "url": "/netdb-web/static/js/main.5a8fd7f7.chunk.js"
   },
   {
     "revision": "595802fbfcdd0cb4fa08",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/netdb-web/precache-manifest.4f06165094f7992c32ff2b094cae7009.js"
+  "/netdb-web/precache-manifest.914175de209d20770fa7b8d07683ca26.js"
 );
 
 self.addEventListener('message', (event) => {
